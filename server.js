ecae5d63db90b9ad9b63088b9ef97fd1a363ccb8
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-08-23 20:43:45 +0100

    Set up passport

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 "use strict";
 require("dotenv").config();
-const app = require("./app");
+const express = require("express");
+const app = express();
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const myDB = require("./connection");
+const session = require("express-session");
+const passport = require("passport");
 
 fccTesting(app); //For FCC testing purposes
+app.set("view engine", "pug");
+app.set("views", "./views/pug");
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.route("/").get((req, res) => {
+  res.render("index", { title: "Hello", message: "Please log in" });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
