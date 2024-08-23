8bdfea35d8fac9a4706dc0069189df0ca80f0902
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-08-23 20:31:07 +0100

    Initialize session and passport

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 const app = require("./app");
 const myDB = require("./connection");
 
+const session = require("express-session");
+const passport = require("passport");
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Listening on port " + PORT);
