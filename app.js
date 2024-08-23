d729fe20374b45591a446fb6fe65caf155e87138
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-08-23 14:11:03 +0100

    Set up Passport

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const session = require("express-session");
+const passport = require("passport");
 
 const app = express();
 
@@ -9,6 +11,16 @@ app.set("views", "./views/pug");
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.route("/").get((req, res) => {
   res.render("index", { title: "Hello", message: "Please log in" });
