7839d1d58ce47b7830e5c3c291a43e611da9acb8
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-08-23 20:27:47 +0100

    Initialize session and passport

--- app.js
+++ app.js
@@ -4,24 +4,12 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const session = require("express-session");
 const passport = require("passport");
 
+fccTesting(app); //For FCC testing purposes
 app.set("view engine", "pug");
 app.set("views", "./views/pug");
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: true,
-    cookie: { secure: false },
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-fccTesting(app); //For FCC testing purposes
 
 app.route("/").get((req, res) => {
   res.render("index", { title: "Hello", message: "Please log in" });
